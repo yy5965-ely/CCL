f39cfd40dbba21c1f10b8209a44b063df03c5474
Yy5965 <yy5965@nyu.edu>
2026-04-24 14:45:44 +0800

    second-website

--- Project-b/sketch.js
+++ Project-b/sketch.js
@@ -1,44 +1,173 @@
-let myPlant;
-let imgPlant;
+let items = [];
+
+let imgSky, imgGrass, imgPond, imgCherry, imgMaple, imgTree1, imgTree2;
+let imgCat, imgSquirrel, imgSunflower, imgWildflower;
 
 function preload() {
-  imgPlant = loadImage('cherry.png');
+  imgSky = loadImage('sky.png'); 
+  imgGrass = loadImage('lawn.png');
+  imgPond = loadImage('pond.png');
+  
+  imgCherry = loadImage('cherry.png');
+  imgMaple = loadImage('maple.png');
+  imgTree1 = loadImage('tree1.png'); 
+  imgTree2 = loadImage('tree2.png');
+  imgCat = loadImage('cat.png');
+  imgSquirrel = loadImage('squirrel.png');
+  imgSunflower = loadImage('sunflower.png');
+  imgWildflower = loadImage('flower.png');
 }
 
 function setup() {
   createCanvas(1500, 800);
-  myPlant = new Plant(width / 2, height / 2);
+  
+  items.push(new GardenItem(imgSky, width / 2, height / 2, 1600));
+
+  items.push(new GardenItem(imgGrass, width / 2, height / 2 + 150, 1500));
+  items.push(new GardenItem(imgPond, 950, 580, 480));
+  
+  items.push(new GardenItem(imgTree1, 80, 280, 650)); 
+  items.push(new GardenItem(imgTree2, 1000, 280, 650)); 
+  items.push(new GardenItem(imgTree1, 1350, 280, 650)); 
+  items.push(new GardenItem(imgCherry, 600, 250, 950));
+  items.push(new GardenItem(imgMaple, 1250, 400, 650));
+  
+  items.push(new GardenItem(imgSunflower, 150, 380, 300));
+  items.push(new GardenItem(imgWildflower, 1250, 680, 500));
+  items.push(new GardenItem(imgWildflower, 900, 720, 350));
+  
+  items.push(new GardenItem(imgCat, 400, 500, 600));
+  items.push(new GardenItem(imgSquirrel, 750, 420, 200));
 }
 
 function draw() {
   background(30); 
   
-  myPlant.update();
-  myPlant.display();
+  let totalHealth = 0;
+  let count = 0;
+
+  for (let i = 0; i < items.length; i++) {
+    items[i].update();
+    items[i].display();
+    
+    if (!items[i].isBackgroundElement) {
+      totalHealth += items[i].health;
+      count++;
+    }
+  }
+  
+  let avgHealth = count > 0 ? totalHealth / count : 0;
+  
+  drawGlobalGrid(avgHealth);
   
   fill(255);
   noStroke();
   textAlign(CENTER);
-  text("Click the plant to care for it.", width/2, height - 30);
-  text("Health: " + floor(myPlant.health), width/2, height - 10);
+  text("Click on any element to care for it.", width / 2, height - 30);
+  textAlign(LEFT);
+  text("System Health: " + floor(avgHealth) + "%", 20, 30);
 }
 
 function mousePressed() {
-  myPlant.care(mouseX, mouseY);
+  for (let i = 0; i < items.length; i++) {
+    items[i].care(mouseX, mouseY);
+  }
 }
 
-class Plant {
+function drawGlobalGrid(avgHealth) {
+  if (avgHealth >= 40) return; 
+
+  let maxDist = dist(0, 0, width / 2, height / 2) + 100; 
+  let safeRadius = map(avgHealth, 40, 0, maxDist, -300); 
+  
+  let gridSize = 30; 
+  rectMode(CENTER);
+  
+  for (let x = -gridSize; x <= width + gridSize; x += gridSize) {
+    for (let y = -gridSize; y <= height + gridSize; y += gridSize) {
+      let d = dist(x, y, width / 2, height / 2);
+      
+      if (d >= safeRadius) { 
+        let gridAlpha = map(d - safeRadius, 0, 200, 40, 255);
+        gridAlpha = constrain(gridAlpha, 0, 255);
+        
+        fill(70, 70, 70, gridAlpha * random(0.85, 1));
+        stroke(30, 30, 30, gridAlpha);
+        rect(x, y, gridSize * 0.95, gridSize * 0.95);
+      }
+    }
+  }
+}
+
+class AshParticle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.size = 500;
-    this.health = 100;
+    this.vx = random(-2.5, -0.5); 
+    this.vy = random(-1.0, 1.0); 
+    this.life = 255;
+    this.size = random(2, 4);
+    
+    let shade = random(50, 150);
+    this.color = color(shade, shade, shade);
   }
 
   update() {
-    this.health -= 0.05; 
-    if (this.health < 0) {
-      this.health = 0;
+    this.x += this.vx;
+    this.y += this.vy;
+    this.life -= random(2, 4); 
+  }
+
+  display() {
+    noStroke();
+    fill(red(this.color), green(this.color), blue(this.color), this.life);
+    rectMode(CENTER);
+    rect(this.x, this.y, this.size, this.size);
+  }
+
+  isDead() {
+    return this.life <= 0;
+  }
+}
+
+class GardenItem {
+  constructor(img, x, y, size) {
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.health = 100 + random(-5, 5); 
+    this.isBackgroundElement = (img === imgGrass || img === imgSky);
+    this.particles = []; 
+  }
+
+  update() {
+    if (!this.isBackgroundElement) {
+      this.health -= 0.01; 
+      if (this.health < 0) {
+        this.health = 0;
+      }
+
+      let imgW = this.size;
+      let imgH = this.img.width > 0 ? this.size * (this.img.height / this.img.width) : this.size;
+
+      if (this.health < 40 && this.health > 10) {
+        if (random() < 0.2) { 
+          let numParticles = floor(random(2, 5));
+          for (let k = 0; k < numParticles; k++) {
+            let px = this.x + random(-imgW / 2, imgW / 2); 
+            let py = this.y + random(-imgH / 2, imgH / 2); 
+            this.particles.push(new AshParticle(px, py));
+          }
+        }
+      }
+    }
+
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].update();
+      if (this.particles[i].isDead()) {
+        this.particles.splice(i, 1);
+      }
     }
   }
 
@@ -53,44 +182,28 @@ class Plant {
   }
 
   display() {
+    for (let p of this.particles) {
+      p.display();
+    }
+
     push();
-    let picAlpha = map(this.health, 0, 100, 0, 1); 
-    drawingContext.globalAlpha = picAlpha; 
-    
     imageMode(CENTER);
+    
     let imgW = this.size;
     let imgH = this.size;
-    if (imgPlant.width > 0) {
-      imgH = this.size * (imgPlant.height / imgPlant.width);
+    if (this.img.width > 0) {
+      imgH = this.size * (this.img.height / this.img.width);
     }
-    image(imgPlant, this.x, this.y, imgW, imgH);
+
+    if (!this.isBackgroundElement && this.health < 40) {
+      let alphaValue = map(this.health, 40, 15, 255, 0);
+      alphaValue = constrain(alphaValue, 0, 255);
+      tint(255, alphaValue); 
+    }
+
+    image(this.img, this.x, this.y, imgW, imgH);
+    
+    noTint(); 
     pop();
-
-    let grid = map(this.health, 80, 0, 0, 255);
-    grid = constrain(grid, 0, 255); 
-
-    if (grid > 0 && imgPlant.width > 0) {
-      fill(150, 150, 150, grid);
-      stroke(80, 80, 80, grid);
-      rectMode(CENTER);
-      
-      let boxSize = map(this.health, 80, 0, 20, 8);
-      boxSize = constrain(boxSize, 8, 20);
-
-      for (let i = -imgW / 2; i < imgW / 2; i += boxSize) {
-        for (let j = -imgH / 2; j < imgH / 2; j += boxSize) {
-          let px = floor(map(i, -imgW / 2, imgW / 2, 0, imgPlant.width));
-          let py = floor(map(j, -imgH / 2, imgH / 2, 0, imgPlant.height));
-          
-          let c = imgPlant.get(px, py);
-          
-          if (c && c[3] > 50 && (c[0] < 240 || c[1] < 240 || c[2] < 240)) {
-            let offsetX = random(-1.5, 1.5);
-            let offsetY = random(-1.5, 1.5);
-            rect(this.x + i + offsetX, this.y + j + offsetY, boxSize * 0.8);
-          }
-        }
-      }
-    }
   }
 }
---
+++ my-second-website/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  let canvas = createCanvas(800, 500);
+  canvas.parent("p5-canvas-containers")
+}
+
+function draw() {
+  background(220);
+}
